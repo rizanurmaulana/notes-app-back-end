0ea62cc6f1f61e2522953513927d7303011d152c
rizanurmaulana <rizanurmaulana2810@gmail.com>
2023-06-23 16:09:54 +0700

    data validation implemented

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesServices');
+const NoteValidator = require('./validator/notes/index');
 
 const init = async () => {
   const notesService = new NotesService();
@@ -18,6 +19,7 @@ const init = async () => {
     plugin: notes,
     options: {
       service: notesService,
+      validator: NoteValidator,
     },
   });
 

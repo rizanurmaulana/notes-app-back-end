1928154b20d894587f4edeb4f4467990e573daf8
rizanurmaulana <rizanurmaulana2810@gmail.com>
2023-07-15 14:33:14 +0700

    user registration implemented

--- src/validator/notes/index.js
+++ src/validator/notes/index.js
@@ -4,7 +4,8 @@ const { NotePayloadSchema } = require('./schema');
 const NotesValidator = {
   validateNotePayload: (payload) => {
     const validationResult = NotePayloadSchema.validate(payload);
-    if(validationResult.error) {
+
+    if (validationResult.error) {
       throw new InvariantError(validationResult.error.message);
     }
   },
